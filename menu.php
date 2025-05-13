d5ec2b6ac680b39ba73bc0c69fd4fe565a8b94be
HaziqShah11 <haziqshah368@gmail.com>
2025-05-13 10:29:40 +0800

    test

---
+++ menu.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">OpenSenseMap</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav ms-auto">
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="locationDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Locations
+          </a>
+          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="locationDropdown">
+            <li><a class="dropdown-item" href="index.php?location=barcelona">Barcelona</a></li>
+            <li><a class="dropdown-item" href="index.php?location=brunei">Brunei</a></li>
+            <li><a class="dropdown-item" href="index.php?location=singapore">Singapore</a></li>
+            <li><a class="dropdown-item" href="index.php?location=london">London</a></li>
+            <li><a class="dropdown-item" href="index.php?location=newyork">New York</a></li>
+          </ul>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
